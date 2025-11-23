10ed42ce72fd503b4192b8e4f21a2b75d603d6d4
Konrad Szydlo <konrad_szydlo@o2.pl>
2025-11-23 21:20:03 +0100

    fix formatting

--- src/com/apriary/pages/app.clj
+++ src/com/apriary/pages/app.clj
@@ -7,16 +7,16 @@
 (defn app [{:keys [session biff/db] :as _ctx}]
   (let [{:user/keys [email]} (xt/entity db (:uid session))]
     (ui/page
-      {}
-      [:div "Signed in as " email ". "
-       (biff/form
-         {:action "/auth/signout"
-          :class "inline"}
-         [:button.text-blue-500.hover:text-blue-800 {:type "submit"}
-          "Sign out"])
-       "."]
-      [:.h-6]
-      [:div "Nothing here yet."])))
+     {}
+     [:div "Signed in as " email ". "
+      (biff/form
+       {:action "/auth/signout"
+        :class "inline"}
+       [:button.text-blue-500.hover:text-blue-800 {:type "submit"}
+        "Sign out"])
+      "."]
+     [:.h-6]
+     [:div "Nothing here yet."])))
 
 (def module
   {:routes ["/app" {:middleware [mid/wrap-signed-in]}
--- src/com/apriary/pages/home.clj
+++ src/com/apriary/pages/home.clj
@@ -11,42 +11,42 @@
 
 (defn home-page [{:keys [recaptcha/site-key params] :as ctx}]
   (ui/page
-    (assoc ctx ::ui/recaptcha true)
-    (biff/form
-      {:action "/auth/send-link"
-       :id "signup"
-       :hidden {:on-error "/"}}
-      (biff/recaptcha-callback "submitSignup" "signup")
-      [:h2.text-2xl.font-bold (str "Sign up for " settings/app-name)]
-      [:.h-3]
-      [:.flex
-       [:input#email {:name "email"
-                      :type "email"
-                      :autocomplete "email"
-                      :placeholder "Enter your email address"}]
-       [:.w-3]
-       [:button.btn.g-recaptcha
-        (merge (when site-key
-                 {:data-sitekey site-key
-                  :data-callback "submitSignup"})
-               {:type "submit"})
-        "Sign up"]]
-      (when-some [error (:error params)]
-        [:<>
-         [:.h-1]
-         [:.text-sm.text-red-600
-          (case error
-            "recaptcha" (str "You failed the recaptcha test. Try again, "
-                             "and make sure you aren't blocking scripts from Google.")
-            "invalid-email" "Invalid email. Try again with a different address."
-            "send-failed" (str "We weren't able to send an email to that address. "
-                               "If the problem persists, try another address.")
-            "There was an error.")]])
-      [:.h-1]
-      [:.text-sm "Already have an account? " [:a.link {:href "/signin"} "Sign in"] "."]
-      [:.h-3]
-      biff/recaptcha-disclosure
-      email-disabled-notice)))
+   (assoc ctx ::ui/recaptcha true)
+   (biff/form
+    {:action "/auth/send-link"
+     :id "signup"
+     :hidden {:on-error "/"}}
+    (biff/recaptcha-callback "submitSignup" "signup")
+    [:h2.text-2xl.font-bold (str "Sign up for " settings/app-name)]
+    [:.h-3]
+    [:.flex
+     [:input#email {:name "email"
+                    :type "email"
+                    :autocomplete "email"
+                    :placeholder "Enter your email address"}]
+     [:.w-3]
+     [:button.btn.g-recaptcha
+      (merge (when site-key
+               {:data-sitekey site-key
+                :data-callback "submitSignup"})
+             {:type "submit"})
+      "Sign up"]]
+    (when-some [error (:error params)]
+      [:<>
+       [:.h-1]
+       [:.text-sm.text-red-600
+        (case error
+          "recaptcha" (str "You failed the recaptcha test. Try again, "
+                           "and make sure you aren't blocking scripts from Google.")
+          "invalid-email" "Invalid email. Try again with a different address."
+          "send-failed" (str "We weren't able to send an email to that address. "
+                             "If the problem persists, try another address.")
+          "There was an error.")]])
+    [:.h-1]
+    [:.text-sm "Already have an account? " [:a.link {:href "/signin"} "Sign in"] "."]
+    [:.h-3]
+    biff/recaptcha-disclosure
+    email-disabled-notice)))
 
 (defn link-sent [{:keys [params] :as ctx}]
   (ui/page
@@ -74,11 +74,11 @@
       "Sign in"]])
    (when-some [error (:error params)]
      [:<>
-       [:.h-1]
-       [:.text-sm.text-red-600
-        (case error
-          "incorrect-email" "Incorrect email address. Try again."
-          "There was an error.")]])))
+      [:.h-1]
+      [:.text-sm.text-red-600
+       (case error
+         "incorrect-email" "Incorrect email address. Try again."
+         "There was an error.")]])))
 
 (defn signin-page [{:keys [recaptcha/site-key params] :as ctx}]
   (ui/page
@@ -143,11 +143,11 @@
       "Sign in"]])
    (when-some [error (:error params)]
      [:<>
-       [:.h-1]
-       [:.text-sm.text-red-600
-        (case error
-          "invalid-code" "Invalid code."
-          "There was an error.")]])
+      [:.h-1]
+      [:.text-sm.text-red-600
+       (case error
+         "invalid-code" "Invalid code."
+         "There was an error.")]])
    [:.h-3]
    (biff/form
     {:action "/auth/send-code"
